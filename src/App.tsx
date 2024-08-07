e514065cdfae66d134d0f5f7aa550975a4bb25d3
Ariel <146725507+Baterista15@users.noreply.github.com>
2024-08-07 14:35:59 -0300

    Ariel_Completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Produto } from './components/Home'
-import ImagemProduto from './components/imagem_Produto'
+import {ImagemProduto} from './components/imagem_Produto'
 
 export default function App() {
-  return <Produto />
-  return <ImagemProduto />
+  return <>
+    <Produto />
+    <ImagemProduto />
+  </>
 }
