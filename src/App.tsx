b783d51201d37f306bbaaeb5f52a68896929a095
Ariel <146725507+Baterista15@users.noreply.github.com>
2024-08-06 21:24:29 -0300

    commit-Ariel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Produto } from './components/Home'
+import ImagemProduto from './components/imagem_Produto'
 
 export default function App() {
   return <Produto />
+  return <ImagemProduto />
 }
---
+++ src/components/imagem_Produto.tsx
@@ -0,0 +1,24 @@
+function ImagemProduto() {
+    return (
+      <>
+        <div className=" flex gap-x-6 py-4">
+              <div className='flex flex-col p-2 pt-0 gap-y-8'>
+                <div className="hover:scale-125 bg-[#F9F1E7] flex flex-content-center items-center w-[80px] h-[80px] rounded-[10px] "><a href="#"><img src="Outdoor sofa set 2.png" alt="Outdoor Sofa Set 2" className='mb-2'/></a></div>
+  
+                <div className='hover:scale-125 bg-[#F9F1E7] justify-center w-[80px] h-[80px] rounded-[10px]'><a href="#"><img src="Outdoor sofa set_2 1.png" alt="Outdoor sofa set_2 1.png" /></a></div>
+  
+                <div className="hover:scale-125 bg-[#F9F1E7] flex items-center justify-center w-[80px] h-[80px] rounded-[10px] "><a href="#"><img src="Stuart sofa 1.png" alt="Stuart Sofa 1" /></a></div>
+  
+                <div className="hover:scale-125 bg-[#F9F1E7] flex items-center justify-center w-[80px] h-[80px] rounded-[10px] "><a href="#"><img src="Maya sofa three seater (1) 1.png" alt="Maya Sofa Three Seater" /></a></div>
+              </div>
+  
+            <div className="flex rounded-[10px] bg-[#F9F1E7] items-center justify-center w-[423px] h-[500px]">  
+            <a href="#"><img src='Asgaard sofa 3.png' alt="Asgaard Sofa" className="w-[481px] h-[391px]"/></a>
+            </div>
+  
+        </div>
+      </>
+    );
+  }
+  
+  export default ImagemProduto;
