693f80f0bd2acbc86c5c3a63c62b315f63756b02
Ariel <146725507+Baterista15@users.noreply.github.com>
2024-08-07 18:44:31 -0300

    commit-ariel

--- src/components/imagem_Produto.tsx
+++ src/components/imagem_Produto.tsx
@@ -11,7 +11,7 @@ export function ImagemProduto() {
               <div className='flex flex-col p-2 pt-0 gap-y-8'>
                 <div className="hover:scale-125 bg-[#F9F1E7] flex flex-content-center items-center w-[80px] h-[80px] rounded-[10px] "><a href="#"><img src={Outdoorsofaset2} alt="Outdoor Sofa Set 2" className='mb-2'/></a></div>
   
-                <div className='hover:scale-125 bg-[#F9F1E7] justify-center w-[80px] h-[80px] rounded-[10px]'><a href="#"><img src={Outdoorsofaset1}alt="Outdoorsofaset1" /></a></div>
+                <div className='hover:scale-125 bg-[#F9F1E7] justify-center w-[80px] h-[80px] rounded-[10px]'><a href="#"><img src={Outdoorsofaset1 }alt="Outdoorsofaset1" /></a></div>
   
                 <div className="hover:scale-125 bg-[#F9F1E7] flex items-center justify-center w-[80px] h-[80px] rounded-[10px] "><a href="#"><img src={Stuartsofa1} alt="Stuart Sofa 1" /></a></div>
   
